a03f652b5bdd97ff3d6aed71096cd917d1fc6a1d
Loay94 <jodhloay@gmail.com>
2021-01-30 13:24:58 +0200

    new assignment

---
+++ React/firstproject/src/components/test.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class SomeClassComponent extends Component {
+    render() {
+        return <div>This is our first class component.</div>;
+    }
+}
+    
+export default SomeClassComponent;
