a10e66f9692b7fd6137e5ca97033ab3c5439b533
Loay94 <jodhloay@gmail.com>
2021-01-30 08:27:37 +0200

    th JSX first assignment

---
+++ React/firstproject/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+      <header className="App-header">
+        <h1>Hello Dojo !</h1>
+        <h2>Things I need to do :</h2>
+        <ul>
+          <li><h3>Learn React</h3></li>
+          <li><h3>Ckimb Mt  Everest</h3></li>
+          <li><h3>Run a Marathon </h3></li>
+          <li><h3>Feed The Dogs</h3></li>
+        </ul>
+        </header>
+    
+  );
+}
+
+export default App;
