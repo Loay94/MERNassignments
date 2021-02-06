767fee10b4d6a50b956e71f5a6261b2c88d752df
Loay94 <jodhloay@gmail.com>
2021-02-06 18:49:01 +0200

    new assigment

--- React/lukeapiwalker/src/App.js
+++ React/lukeapiwalker/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <form>
-      <label >Serch for : </label>
-        <select id="cars">
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="opel">Opel</option>
-        <option value="audi">Audi</option>
+      <label >Search for : </label>
+        <select id="types">
+        <option value="planet">Planets</option>
+        <option value="pepole">Pepole</option>
       </select>
+      <label> ID : </label>
+      <input type="number" />
+      <button >Search</button>
       </form>
     </div>
   );
