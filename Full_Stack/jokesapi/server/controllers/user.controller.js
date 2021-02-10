3b9b9b17e6dea441c0f9d26e667d394455955ad5
Loay94 <jodhloay@gmail.com>
2021-02-10 12:51:21 +0200

    jokes_api

---
+++ Full_Stack/jokesapi/server/controllers/user.controller.js
@@ -0,0 +1,31 @@
+const Jokes = require("../models/user.model");
+
+module.exports.findAllUsers = (req, res) => {
+  Jokes.find()
+    .then(allDaUsers => res.json({ users: allDaUsers }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.findOneSingleUser = (req, res) => {
+	Jokes.findOne({ _id: req.params.id })
+		.then(oneSingleUser => res.json({ user: oneSingleUser }))
+		.catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.createNewUser = (req, res) => {
+  Jokes.create(req.body)
+    .then(newlyCreatedUser => res.json({ user: newlyCreatedUser }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.updateExistingUser = (req, res) => {
+  Jokes.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(updatedUser => res.json({ user: updatedUser }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.deleteAnExistingUser = (req, res) => {
+  Jokes.deleteOne({ _id: req.params.id })
+    .then(result => res.json({ result: result }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
